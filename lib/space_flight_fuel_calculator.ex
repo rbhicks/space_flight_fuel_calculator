d30f804daf2ea4a55358217d2976e1ceae9d8183
Ryan B. Hicks <r.b.hicks@gmail.com>
2022-10-25 20:38:35 +0200

    initial commit.

---
+++ lib/space_flight_fuel_calculator.ex
@@ -0,0 +1,18 @@
+defmodule SpaceFlightFuelCalculator do
+  @moduledoc """
+  Documentation for `SpaceFlightFuelCalculator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SpaceFlightFuelCalculator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/space_flight_fuel_calculator_test.exs
@@ -0,0 +1,8 @@
+defmodule SpaceFlightFuelCalculatorTest do
+  use ExUnit.Case
+  doctest SpaceFlightFuelCalculator
+
+  test "greets the world" do
+    assert SpaceFlightFuelCalculator.hello() == :world
+  end
+end
