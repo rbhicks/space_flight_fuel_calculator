7ab55aad9a13871a3ffd90de5bb195c754d27236
Ryan B. Hicks <r.b.hicks@gmail.com>
2022-10-25 21:25:51 +0200

    added multi flight test using data from the test description.

--- test/space_flight_fuel_calculator_test.exs
+++ test/space_flight_fuel_calculator_test.exs
@@ -4,4 +4,33 @@ defmodule SpaceFlightFuelCalculatorTest do
   test "land Apollo 11 CSM on Earth" do
     assert SpaceFlightFuelCalculator.calculate(28801, [{:land, 9.807}]) == 13447
   end
+
+  test "Apollo 11" do
+    assert SpaceFlightFuelCalculator.calculate(28801, [
+             {:launch, 9.807},
+             {:land, 1.62},
+             {:launch, 1.62},
+             {:land, 9.807}
+           ]) == 51898
+  end
+
+  test "Mission on Mars" do
+    assert SpaceFlightFuelCalculator.calculate(14606, [
+             {:launch, 9.807},
+             {:land, 3.711},
+             {:launch, 3.711},
+             {:land, 9.807}
+           ]) == 33388
+  end
+
+  test "Passenger ship" do
+    assert SpaceFlightFuelCalculator.calculate(75432, [
+             {:launch, 9.807},
+             {:land, 1.62},
+             {:launch, 1.62},
+             {:land, 3.711},
+             {:launch, 3.711},
+             {:land, 9.807}
+           ]) == 212_161
+  end
 end
