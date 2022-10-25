dbd665f6779951676631049c405635aea1bcb181
Ryan B. Hicks <r.b.hicks@gmail.com>
2022-10-25 21:11:22 +0200

    implemented base calculation algorithm. i.e., for a single flight.

--- lib/space_flight_fuel_calculator.ex
+++ lib/space_flight_fuel_calculator.ex
@@ -1,18 +1,56 @@
 defmodule SpaceFlightFuelCalculator do
-  @moduledoc """
-  Documentation for `SpaceFlightFuelCalculator`.
-  """
+  def calculate(spacecraft_weight, [{flight_directive, acceleration_due_to_gravity}]) do
+    next_fuel_weight =
+      get_fuel_weight(
+        spacecraft_weight,
+        flight_directive,
+        acceleration_due_to_gravity
+      )
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SpaceFlightFuelCalculator.hello()
-      :world
-
-  """
-  def hello do
-    :world
+      calculate_for_fuel_weight(
+        flight_directive,
+        acceleration_due_to_gravity,
+        0,
+        next_fuel_weight
+      )
   end
+
+  def calculate_for_fuel_weight(
+        flight_directive,
+        acceleration_due_to_gravity,
+        total_fuel_weight,
+        last_fuel_weight
+      )
+      when last_fuel_weight > 0 do
+    next_fuel_weight =
+      get_fuel_weight(last_fuel_weight, flight_directive, acceleration_due_to_gravity)
+
+    calculate_for_fuel_weight(
+      flight_directive,
+      acceleration_due_to_gravity,
+      total_fuel_weight + last_fuel_weight,
+      next_fuel_weight
+    )
+  end
+
+  def calculate_for_fuel_weight(
+        _flight_directive,
+        _acceleration_due_to_gravity,
+        total_fuel_weight,
+        last_fuel_weight
+      )
+      when last_fuel_weight <= 0 do
+    total_fuel_weight
+  end
+
+  defp get_fuel_weight(weight, flight_directive, acceleration_due_to_gravity) do
+    (weight * acceleration_due_to_gravity * get_flight_directive_multiplier(flight_directive) -
+       get_flight_directive_minuend(flight_directive))
+    |> trunc
+  end
+
+  def get_flight_directive_multiplier(:launch), do: 0.042
+  def get_flight_directive_multiplier(:land), do: 0.033
+  def get_flight_directive_minuend(:launch), do: 33
+  def get_flight_directive_minuend(:land), do: 42
 end
